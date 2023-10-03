86b6866183c556ab3e1bc404f09fba7f5a7460e5
ohunewald <oliver.hunewald@lih.lu>
2023-10-03 11:20:58 +0200

    cleaned utils section

--- R/utils.R
+++ R/utils.R
@@ -7,29 +7,7 @@ subsetdf <- function(markers) {
 
   return(df[1:20, 1:8])
 }
-# kmeansTH <- function(df) {
-#   # browser()
-#   marker <- colnames(df)
-#
-#   th <- as.data.frame(marker)
-#   th$value <- 0.0
-#
-#   colnames(th) <- c('cell', 'value')
-#
-#   # M <- as.matrix(df %>% normalize01())
-#   M <- as.matrix(df)
-#
-#   for (m in th$cell) {
-#
-#     k<-2
-#     z <- Ckmeans.1d.dp(M[, m], 2)
-#     midpoints <- ahist(z, style="midpoints", data=M[,m], plot=FALSE)$breaks[2:k]
-#
-#     th[th$cell == m, "value"] <- round(midpoints, 3)
-#   }
-#
-#   return(th)
-# }
+
 
 kmeansTH <- function(df) {
   th <- data.frame(cell = colnames(df), value = 0.0, color = "blue")
@@ -90,20 +68,12 @@ filterHM <- function(DF,posList, negList, th) {
     posTH <- th$value[th$cell %in% posList]
     negTH <- th$value[th$cell %in% negList]
     # first reduce by the positive markers
-    # for (i in 1:length(posList)) {
-    #   marker <- posList[i]
-    #   DF <- DF[DF[marker] > posTH[i], ]
-    # }
 
     for (i in 1:length(posList)) {
       marker <- posList[i]
       DF <- DF[DF[marker] > th[marker,]$value, ]
     }
     # next reduce by the negative markers
-    # for (i in 1:length(negList)) {
-    #   marker <- negList[i]
-    #   DF <- DF[DF[marker] < negTH[i], ]
-    # }
 
     for (i in 1:length(negList)) {
       marker <- negList[i]
@@ -134,22 +104,7 @@ filterAnnotation <- function(hm_tmp,at_tmp) {
 set_ptname <- function(ptname, df) {
   df$phenotype <- ptname
 }
-# setPhenotypeName <- function(markers, s, ph_name) {
-#
-#   if (s == "pos") {
-#     for (n in markers) {
-#       ph_name <- paste0(ph_name, n, '+')
-#     }
-#     return(ph_name)
-#   }
-#   if (s == "neg") {
-#     for (n in markers) {
-#       ph_name <- paste0(ph_name, n, '-')
-#     }
-#     return(ph_name)
-#   }
-#
-# }
+
 
 setPhenotypeName <- function(markers, s, ph_name) {
   if (s == "pos") {
@@ -178,24 +133,6 @@ getMarkerDistDF <- function(marker, myScale){
   return(marker_expr)
 }
 
-# # a function to add a new row for nodes and edges
-# add_node <- function(graph,parent,name,pos_m,neg_m,color,freq,nrows) {
-#
-#   next_id <- max(graph$nodes$id) + 1
-#   parent_row <- graph$nodes %>% filter(label == parent)
-#   # node_level <- paste0(parent_row$id,".",2)
-#   graph$nodes <- graph$nodes %>% add_row(id = next_id,
-#                                          label = name,
-#                                          pm = pos_m,
-#                                          nm = neg_m,
-#                                          color = color,
-#                                          nFreq = freq,
-#                                          nRows = nrows)
-#
-#   graph$edges <- graph$edges %>% add_row(from = next_id, to = parent_row$id)
-#
-#   return(graph)
-# }
 
 # a function to add a new row for nodes and edges
 add_node <- function(graph,parent,name,pos_m,neg_m,color) {
@@ -252,7 +189,6 @@ all_my_children <- function(graph_data, node_id) {
 
 
 }
-
 
 # define recursive function to delete a node and all its children
 delete_leaf_node <- function(graph_data, node_id) {
