54a572d43091568906493aba7f7b07aed3b833c4
ohunewald <oliver.hunewald@lih.lu>
2024-01-15 08:56:11 +0100

    clean-up

--- R/app.R
+++ R/app.R
@@ -1,19 +1,3 @@
-options(repos = "https://cran.r-project.org/")
-
-# List of packages you want to check and install if needed
-# packages_to_install <- c("shiny", "DT", "ggplot2", "matrixStats", "tidyverse", "stats",
-#                          "pheatmap", "Ckmeans.1d.dp", "umap", "RColorBrewer", "shinydashboard",
-#                          "shinyWidgets", "visNetwork", "glue", "purrr", "reshape2", "mousetrap")
-# 
-# # Check if each package is already installed, and install if not
-# for (package in packages_to_install) {
-#   if (!require(package, character.only = TRUE)) {
-#     install.packages(package)
-#     library(package, character.only = TRUE)
-#   }
-# }
-
-
 
 library("shiny")
 library("DT")
