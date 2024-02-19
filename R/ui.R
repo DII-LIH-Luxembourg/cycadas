cfce679ca0109352940f204435845b5939d58824
Joseph Longworth <josephlongworth@gmail.com>
2024-02-19 22:45:54 +0100

    change settings UI for condensed viewing

--- R/ui.R
+++ R/ui.R
@@ -33,196 +33,44 @@ ui <- dashboardPage(
 
               ),
       tabItem(tabName = "settings",
-              fluidRow(column(
-                
-                width = 12,
-                tabBox(
-                  width = NULL,
-                  title = "Data upload",
-                  id = "tab-required",
-                  height = "450px",
-                  tabPanel(
-                    "Required",
-                    "Required",
-                    box(
-                      title = "Upload Marker-expression",
-                      collapsible = TRUE,
-                      solidHeader = TRUE,
-                      status = "info",
-                      width = NULL,
-                      collapsed = F,
-                      fileInput(
-                        "fMarkerExpr",
-                        "Choose CSV File",
-                        multiple = FALSE,
-                        accept = c("text/csv",
-                                   "text/comma-separated-values,text/plain",
-                                   ".csv")
-                      ),
-                      tags$hr(),
-                      checkboxInput("header", "Header", TRUE),
-                      radioButtons(
-                        "sep",
-                        "Separator",
-                        choices = c(
-                          Comma = ",",
-                          Semicolon = ";",
-                          Tab = "\t"
-                        ),
-                        selected = ",",
-                        inline = T
-                      )
-                    ),
-                    box(
-                      title = "Upload cluster_freq",
-                      collapsible = TRUE,
-                      solidHeader = TRUE,
-                      status = "info",
-                      width = NULL,
-                      collapsed = F,
-                      fileInput(
-                        "cluster_freq",
-                        "Choose CSV File",
-                        multiple = FALSE,
-                        accept = c("text/csv",
-                                   "text/comma-separated-values,text/plain",
-                                   ".csv")
-                      ),
-                      tags$hr(),
-                      checkboxInput("header", "Header", TRUE),
-                      radioButtons(
-                        "sep",
-                        "Separator",
-                        choices = c(
-                          Comma = ",",
-                          Semicolon = ";",
-                          Tab = "\t"
-                        ),
-                        selected = ",",
-                        inline = T
-                      )
-                    )
+              fluidPage(
+                fluidRow(
+                  column(width = 4,
+                         box(title = "Requried",collapsible = TRUE,solidHeader = TRUE,status = "info",width = NULL,collapsed = F,
+                             fileInput("fMarkerExpr","Upload Marker Expressions",
+                                       placeholder = "Choose CSV File",
+                                       multiple = FALSE,
+                                       accept = c("text/csv","text/comma-separated-values,text/plain",".csv")),
+                             fileInput("cluster_freq","Upload Cluster Frequencies",
+                                       placeholder = "Choose CSV File",
+                                       multiple = FALSE,
+                                       accept = c("text/csv","text/comma-separated-values,text/plain",".csv"))
+                         )),
+                  column(width = 4,
+                         box(title = "Optional - Marker-thresholds",collapsible = TRUE,solidHeader = TRUE,status = "warning",width = NULL,collapsed = T,
+                             fileInput("fTH","Choose CSV File",multiple = FALSE,accept = c("text/csv","text/comma-separated-values,text/plain",".csv")),
+                             tags$hr(),
+                             checkboxInput("header", "Header", TRUE),
+                             radioButtons("sep","Separator",choices = c(Comma = ",",Semicolon = ";",Tab = "\t"),selected = ",",inline = T)),
+                         box(title = "Optional - Annotation Tree",collapsible = TRUE,solidHeader = TRUE,status = "warning",width = NULL,collapsed = T,
+                             fileInput("fNodes","Choose Nodes File",multiple = FALSE,accept = c("text/csv","text/comma-separated-values,text/plain",".csv")),
+                             fileInput("fEdges","Choose Edges File",multiple = FALSE,accept = c("text/csv","text/comma-separated-values,text/plain",".csv")),
+                             # fileInput("fAnno","Choose Annotation File",multiple = FALSE,accept = c("text/csv","text/comma-separated-values,text/plain",".csv")),
+                             actionButton("btnImportTree", "Import")),
+                         box(title = "Optional - Metadata",collapsible = TRUE,solidHeader = TRUE,status = "warning",width = NULL,collapsed = T,
+                             fileInput("metadata","Choose CSV File",multiple = F,accept = c("text/csv","text/comma-separated-values,text/plain",".csv"))),
+                         box(title = "Optional - Count Table",collapsible = TRUE,solidHeader = TRUE,status = "warning",width = NULL,collapsed = T,
+                             fileInput("counts_table","Choose CSV File",multiple = F,accept = c("text/csv","text/comma-separated-values,text/plain",".csv")))
                   ),
-                  tabPanel(
-                    "Optional",
-                    "Optional",
-                    box(
-                      title = "Upload Marker-thresholds",
-                      collapsible = TRUE,
-                      solidHeader = TRUE,
-                      status = "info",
-                      width = NULL,
-                      collapsed = F,
-                      fileInput(
-                        "fTH",
-                        "Choose CSV File",
-                        multiple = FALSE,
-                        accept = c("text/csv",
-                                   "text/comma-separated-values,text/plain",
-                                   ".csv")
-                      ),
-                      tags$hr(),
-                      checkboxInput("header", "Header", TRUE),
-                      radioButtons(
-                        "sep",
-                        "Separator",
-                        choices = c(
-                          Comma = ",",
-                          Semicolon = ";",
-                          Tab = "\t"
-                        ),
-                        selected = ",",
-                        inline = T
-                      )
-                    ),
-                    box(
-                      title = "Upload Annotation Tree",
-                      collapsible = TRUE,
-                      solidHeader = TRUE,
-                      status = "info",
-                      width = NULL,
-                      collapsed = F,
-                      fileInput(
-                        "fNodes",
-                        "Choose Nodes File",
-                        multiple = FALSE,
-                        accept = c("text/csv",
-                                   "text/comma-separated-values,text/plain",
-                                   ".csv")
-                      ),
-                      fileInput(
-                        "fEdges",
-                        "Choose Edges File",
-                        multiple = FALSE,
-                        accept = c("text/csv",
-                                   "text/comma-separated-values,text/plain",
-                                   ".csv")
-                      ),
-                      actionButton("btnImportTree", "Import")
-                    ),
-                    # box(
-                    #   title = "Upload Annotation",
-                    #   collapsible = TRUE,
-                    #   solidHeader = TRUE,
-                    #   status = "info",
-                    #   width = NULL,
-                    #   collapsed = F,
-                    #   fileInput(
-                    #     "annTable",
-                    #     "Choose CSV File",
-                    #     multiple = F,
-                    #     accept = c("text/csv",
-                    #                "text/comma-separated-values,text/plain",
-                    #                ".csv")
-                    #   )
-                    # ),
-                    box(
-                      title = "Upload Metadata",
-                      collapsible = TRUE,
-                      solidHeader = TRUE,
-                      status = "info",
-                      width = NULL,
-                      collapsed = F,
-                      fileInput(
-                        "metadata",
-                        "Choose CSV File",
-                        multiple = F,
-                        accept = c("text/csv",
-                                   "text/comma-separated-values,text/plain",
-                                   ".csv")
-                      )
-                    ),
-                    box(
-                      title = "Upload Count Table",
-                      collapsible = TRUE,
-                      solidHeader = TRUE,
-                      status = "info",
-                      width = NULL,
-                      collapsed = F,
-                      fileInput(
-                        "counts_table",
-                        "Choose CSV File",
-                        multiple = F,
-                        accept = c("text/csv",
-                                   "text/comma-separated-values,text/plain",
-                                   ".csv")
-                      )
-                    ) # end box
-                  ), 
-                  tabPanel("Demo Data",
-                           box(
-                             title = "Load Cluster Expression Demo Data",
-                             width = NULL,
-                             actionButton("btnLoadDemoData", "Load")
-                           ),
-                           box(
-                             title = "Load Annotated Demo Data",
-                             width = NULL,
-                             actionButton("btnLoadAnnoData", "Load")
-                           )
+                  column(width = 4,
+                         box(title = "Load Demo Data",collapsible = TRUE,solidHeader = TRUE,status = "success",width = NULL,collapsed = F,
+                             splitLayout(cellWidths = c("50%", "50%"),
+                                         actionButton("btnLoadDemoData", "Unannotated"),
+                                         actionButton("btnLoadAnnoData", "Annotated")))
                   )
                 )
-              ))),
+              )
+              ),
 
       # Tree-Annotation Tab ----------------------------------------------------
       tabItem(tabName = "treeannotation",
